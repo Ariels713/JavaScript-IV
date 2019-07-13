83942cc232513521f1046b9723a767a4d3f6024f
Ariel Rodriguez <ariel_rodriguez@Ariels-Mac-mini.local>
2019-07-12 20:04:05 -0400

    changes to classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,30 +1,13 @@
-/*
+
 // CODE here for your Lambda Classes
-* We have a school to build here! This project will get you used to thinking about classes in JavaScript and building them from a brand new data set.
-* Lambda personnel can be broken down into three different types of `people`.
-  * **Instructors** - extensions of Person
-  * **Students** - extensions of Person
-  * **Project Managers** - extensions of Instructors
-* **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
+// * We have a school to build here! This project will get you used to thinking about classes in JavaScript and building them from a brand new data set.
+// * Lambda personnel can be broken down into three different types of `people`.
+//   * **Instructors** - extensions of Person
+//   * **Students** - extensions of Person
+//   * **Project Managers** - extensions of Instructors
+// * **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
 
-```js
-const fred = new Instructor({
-  name: 'Fred',
-  location: 'Bedrock',
-  age: 37,
-  favLanguage: 'JavaScript',
-  specialty: 'Front-end',
-  catchPhrase: `Don't forget the homies`
-});
-```
-*/
 
-// #### Person
-
-// * First we need a Person class. This will be our `base-class`
-// * Person receives `name` `age` `location` all as props
-// * Person receives `speak` as a method.
-// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
 
 class Person{
     constructor(attributes){
@@ -36,20 +19,6 @@ class Person{
         console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
     }
 }
-
-
-// #### Instructor
-
-// * Now that we have a Person as our base class, we'll build our Instructor class.
-// * Instructor uses the same attributes that have been set up by Person
-// * Instructor has the following unique props:
-//   * `specialty` what the Instructor is good at i.e. 'redux'
-//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-//   * `catchPhrase` i.e. `Don't forget the homies`
-// * Instructor has the following methods:
-//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-
 
 class Instructor extends Person{
     constructor(instructorAttributes){
@@ -64,22 +33,10 @@ class Instructor extends Person{
     }
 
     grade(student, subject){
-        console.log(`${student.name} receives a perfect score on ${subject}`)
+        console.log(`${this.name} receives a perfect score on ${subject}`)
     }
 }
 
-// #### Student
-
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 class Student extends Person{
     constructor(studentAttributes){
@@ -104,17 +61,6 @@ class Student extends Person{
     }
 }
 
-// #### Project Manager
-
-// * Now that we have instructors and students, we'd be nowhere without our PM's
-// * ProjectManagers are extensions of Instructors
-// * ProjectManagers have the following unique props:
-//   * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
-// * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-
 class ProjectManager extends Instructor{
     constructor(PMattributes){
         super(PMattributes);
@@ -132,6 +78,66 @@ class ProjectManager extends Instructor{
 }
 
 
+const elliePerson = new Person({
+    name: `Ellie Arielle`,
+    age: 2,
+    location: `New York, NY`
+  })
+  
+  const laurenPerson = new Person ({
+    name: `Lauren Chil`,
+    age: 29,
+    location: `New York, NY`
+  })
+
+  const ellieInstructor = new Instructor({
+    name: `Ellie Arielle`,
+    age: 2,
+    location: `New York, NY`,
+    speciality: `Javascript`,
+    favLanguage: `Python`,
+    catchPhrase: `I don't want it`
+  })
+
+  const laurenInstructor = new Instructor({
+    name: `LC`,
+    age: 31,
+    location: `West New York, NJ`,
+    speciality: `Ruby`,
+    favLanguage: `C#`,
+    catchPhrase: `Can you rub my feet please`
+  })
+
+  const ellieStudent = new Student({
+    name: `Ellie Rod`,
+    age: 5,
+    location: `New Jersey`,
+    previousBackground: `infant`,
+    className: `WebPT77`,
+    faveSubjects: [`Playdoh`, `Coloring`, `Reading`]
+  })
+
+  const laurenStudent = new Student({
+    name: `Lauren Elyse`,
+    age: 21,
+    location: `Brookyn, NY`,
+    previousBackground: `project manager`,
+    className: `WebPT11`,
+    faveSubjects: [`Organizing`, `Reading`, `Planning`]
+  })
+
+
+  //Code Test
+console.log(elliePerson.speak())
+console.log(laurenPerson.speak())
+console.log(ellieInstructor.grade(elliePerson, 'python'))
+console.log(laurenInstructor.grade(laurenPerson, `Algebra`))
+console.log(ellieStudent.listsSubjects())
+console.log(ellieStudent.PRAassignment('Python'))
+console.log(ellieStudent.sprintChallenge(`Python`))
+console.log(laurenStudent.listsSubjects())
+console.log(laurenStudent.PRAassignment('Python'))
+console.log(laurenStudent.sprintChallenge(`Python`))
 
 // #### Stretch Problem
 
